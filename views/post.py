dbf5d87dfc5a3360fdc96c4e7d353f1cf71ac7dc
Tyler Osmond <140542331+tylerosmond@users.noreply.github.com>
2024-08-28 11:03:31 -0500

    Added retrieve_post function to post module with SQL query to get post details with a JOIN to include the users username. Imported HumanDate component in order to display the posts date correctly, and created the do_GET function in json-server.py to handle response.

--- views/post.py
+++ views/post.py
@@ -2,24 +2,56 @@ import sqlite3
 import json
 from datetime import datetime
 
+
 def create_post(post):
-    with sqlite3.connect('./db.sqlite3') as conn:
+    with sqlite3.connect("./db.sqlite3") as conn:
         conn.row_factory = sqlite3.Row
         db_cursor = conn.cursor()
 
-
-        db_cursor.execute("""
+        db_cursor.execute(
+            """
             Insert into Posts (user_id, category_id, title, publication_date, image_url, content, approved) values (?,?,?,?,?,?,True)
-        """,(
-            post['user_id'],
-            post['category_id'],
-            post['title'],
-            datetime.now(),
-            post['image_url'],
-            post['content']
-        ))
+        """,
+            (
+                post["user_id"],
+                post["category_id"],
+                post["title"],
+                datetime.now(),
+                post["image_url"],
+                post["content"],
+            ),
+        )
 
         new_post_id = db_cursor.lastrowid
 
         return new_post_id
-        
+
+
+def retrieve_post(pk):
+    with sqlite3.connect("./db.sqlite3") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute(
+            """
+        SELECT
+            p.id,
+            p.user_id,
+            p.category_id,
+            p.title,
+            p.publication_date,
+            p.image_url,
+            p.content,
+            p.approved,
+            u.username
+        FROM Posts p
+        JOIN Users u ON u.id = p.user_id
+        WHERE p.id = ?
+        """,
+            (pk,),
+        )
+        query_results = db_cursor.fetchone()
+
+        serialized_post = json.dumps(dict(query_results))
+
+    return serialized_post
