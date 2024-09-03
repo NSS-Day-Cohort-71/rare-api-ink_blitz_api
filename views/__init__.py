b97e3c6dd840d246926b6565729809a1446234c5
Zach Mosher <zmosher4@gmail.com>
2024-09-03 15:35:57 -0500

    added delete_category

--- views/__init__.py
+++ views/__init__.py
@@ -1,5 +1,5 @@
 from .user import create_user, login_user, get_all_users, retrieve_user
-from .category import create_category, list_categories
+from .category import create_category, list_categories, delete_category
 from .post import create_post, retrieve_post, update_post, list_posts, delete_post
 from .tag import create_tag, list_tags, retrieve_tag, update_tag, delete_tags
 from .comment import create_comment, list_comments
